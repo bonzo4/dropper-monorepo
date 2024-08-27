dbc0b4b1abaa44645f88b7570a76aef6901a5d70
hvKalista <alonzo.sabino814@gmail.com>
2024-08-27 16:21:00 -0500

    feat: seed local db

---
+++ lib/utils/seed/codes.ts
@@ -0,0 +1,27 @@
+/**
+ * ! Executing this script will delete all data in your database and seed it with 10 whitelisted_wallets.
+ * ! Make sure to adjust the script to your needs.
+ * Use any TypeScript runner to run this script, for example: `npx tsx seed.ts`
+ * Learn more about the Seed Client by following our guide: https://docs.snaplet.dev/seed/getting-started
+ */
+import { SupabaseClient } from "@supabase/supabase-js";
+import { Database } from "./lib/types";
+import { copycat } from "@snaplet/copycat";
+
+export async function createCodes(supabase: SupabaseClient<Database>) {
+  const accessCodes: { code: string }[] = [];
+
+  for (let i = 0; i < 10; i++) {
+    const code = copycat.word(i);
+    await supabase.from("access_codes").delete().eq("code", code);
+    accessCodes.push({
+      code,
+    });
+  }
+
+  const { error: error2 } = await supabase.from("access_codes").insert(
+    accessCodes,
+  );
+
+  if (error2) console.log(error2);
+}
---
+++ lib/utils/seed/index.ts
@@ -0,0 +1,28 @@
+import { createClient } from "@supabase/supabase-js";
+import { createUsers } from "./users";
+import { whitelistWallets } from "./wallets";
+import { createCodes } from "./codes";
+import { DatabaseTypes } from "@repo/app-types/database";
+
+require("dotenv").config();
+
+const main = async () => {
+  const supabase = createClient<DatabaseTypes>(
+    process.env.SUPABASE_URL!,
+    // Note you might want to use `SUPABASE_ROLE` key here with `auth.admin.signUp` if your app is using email confirmation
+    process.env.SUPABASE_SERVICE_KEY!
+  );
+
+  await createUsers(supabase);
+
+  await supabase.auth.signOut();
+
+  await createCodes(supabase);
+  await whitelistWallets(supabase);
+
+  console.log("Database seeded successfully!");
+
+  process.exit();
+};
+
+main();
