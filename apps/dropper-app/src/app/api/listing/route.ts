792954622bc6f2777c3dbb7f368363bf5576700b
hvKalista <alonzo.sabino814@gmail.com>
2024-08-28 17:18:00 -0500

    feat: listing data

--- apps/dropper-app/src/app/api/listing/route.ts
+++ apps/dropper-app/src/app/api/listing/route.ts
@@ -1,6 +1,17 @@
 import { createSupabaseServer } from "@/lib/supabase/server";
 import { NextRequest } from "next/server";
 
+export type ListingCardData = {
+  ticker: string;
+  name: string;
+  holder_count: number;
+  ath: number;
+  atv: number;
+  icon_url: string;
+  description: string;
+  id: string;
+};
+
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const supabase = createSupabaseServer();
@@ -8,11 +19,15 @@ export async function GET(request: NextRequest) {
   let page = searchParams.get("page");
   let pageNumber = page ? parseInt(page) : 0;
 
-  const query = supabase.from("listings").select("").neq("tx", null);
+  const query = supabase
+    .from("listings")
+    .select("ticker, name, holder_count, ath, atv, icon_url, description, id")
+    .order("last_bump", { ascending: false })
+    .neq("tx_string", null);
 
   if (!isNaN(pageNumber) && pageNumber > 0) {
     const start = (pageNumber - 1) * 12;
-    const end = start + 11;
+    const end = start + 12;
     query.range(start, end);
   } else {
     query.range(0, 11);
