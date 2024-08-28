16d8b15456431d0a1c0d346663f72c57094b2231
hvKalista <alonzo.sabino814@gmail.com>
2024-08-28 17:23:05 -0500

    fix: badges

--- supabase/migrations/20240828221057_badges_enum.sql
+++
@@ -1,7 +0,0 @@
-create type "public"."giveaway_badges" as enum ('GOLD', 'FIST', 'CTO', 'MOON', 'TRENDING', 'PUMP_FUN', 'BNB', 'ETH', 'MATIC', 'SOL', 'BASE');
-
-alter table "public"."giveaways" alter column "badges" set data type giveaway_badges[] using "badges"::giveaway_badges[];
-
-drop type "public"."badges";
-
-
