f008f3c9cc4767b921e99512b258b11c7eb3a4b6
hvKalista <alonzo.sabino814@gmail.com>
2024-08-28 17:14:30 -0500

    feat: listing banners

---
+++ apps/dropper-app/src/app/(user)/listing/components/ListingBannerRowOptions.tsx
@@ -0,0 +1,24 @@
+import { ListingBannerRow } from "@/lib/types/banner";
+import ListingBanner from "./ListingBanner";
+
+type Props = {
+  banners: ListingBannerRow[];
+  width: number;
+};
+
+export default function ListingBannerSliderRow({ banners, width }: Props) {
+  return (
+    <div
+      className="flex flex-row justify-center gap-2 w-full"
+      style={{
+        minWidth: width ? `${width}px` : "1920px",
+      }}
+    >
+      {banners.map((banner, index) => (
+        <div key={index} className="flex flex-row">
+          <ListingBanner banner={banner} />
+        </div>
+      ))}
+    </div>
+  );
+}
