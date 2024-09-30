062cf81c44c1d853d50f831cdfad126976783f36
hvKalista <alonzo.sabino814@gmail.com>
2024-09-29 20:46:26 -0500

    fix: usd_price nullable

--- supabase/migrations/20240929212819_listing_stat_names.sql
+++ supabase/migrations/20240929212819_listing_stat_names.sql
@@ -2,9 +2,9 @@ alter table "public"."listings" drop column "ath";
 
 alter table "public"."listings" drop column "atv";
 
-alter table "public"."listings" add column "usd_price" numeric not null;
+alter table "public"."listings" add column "usd_price" numeric not null default 0.0;
 
-alter table "public"."listings" add column "volume_24h" numeric not null;
+alter table "public"."listings" add column "volume_24h" numeric not null default 0.0;
 
 alter table "public"."listings" alter column "is_cto" set default false;
 
