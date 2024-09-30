eb542ed7743d8f34f799f8599e41a077bb6faf72
hvKalista <alonzo.sabino814@gmail.com>
2024-09-29 21:07:20 -0500

    fix: discord connect

--- apps/dropper-app/src/app/profile/components/ConnectDiscord.tsx
+++ apps/dropper-app/src/app/profile/components/ConnectDiscord.tsx
@@ -24,7 +24,7 @@ export default function ConnectDiscord({ discordAccount }: Props) {
       <h2 className="text-3xl">Connect Discord</h2>
       {!discordAccount && (
         <a
-          href="https://discord.com/oauth2/authorize?client_id=1218366674449989735&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcallback%2Fdiscord&scope=identify"
+          href="https://discord.com/oauth2/authorize?client_id=1218366674449989735&response_type=code&redirect_uri=https%3A%2F%2Fdropper.wtf%2Fauth%2Fcallback%2Fdiscord&scope=identify"
           className=""
         >
           <Button>Link Discord</Button>
